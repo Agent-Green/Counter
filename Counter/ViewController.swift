140f68379f49b808f6830ef77e4448f7d9dd89b7
agent green <a.2000.g1@yandex.ru>
2024-03-26 20:19:19 +0300

    Исправлено по рекомендации ревьюера

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,49 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
-    @IBOutlet weak var mainCounter: UILabel!
-    
-    @IBOutlet weak var minusOneButton: UIButton!
-    
-    @IBOutlet weak var plusOneButton: UIButton!
-    
-    @IBOutlet weak var restartButton: UIButton!
-    
-    @IBOutlet weak var infoCounter: UITextView!
+    // Свойства класса
     
     private var counter = 0
-
-    @IBAction func minusOne(_ sender: Any) {
-        if counter >= 1{
-            counter -= 1
-            mainCounter.text = "\(counter)"
-            infoCounter.text.append("\n[\(Date().formatted())]: значение изменено на -1")
-        } else {
-            infoCounter.text.append("\n[\(Date().formatted())]: попытка уменьшить значение счётчика ниже 0")
-        }
-    }
     
-    @IBAction func restart(_ sender: Any) {
-        mainCounter.text = "0"
-        infoCounter.text.append("\n[\(Date().formatted())]: значение сброшено")
-    }
+    @IBOutlet private weak var mainCounter: UILabel!
     
-    @IBAction func plusOne(_ sender: Any) {
-        counter += 1
-        mainCounter.text = "\(counter)"
-        infoCounter.text.append("\n[\(Date().formatted())]: значение изменено на +1")
-    }
+    @IBOutlet private weak var minusOneButton: UIButton!
+    
+    @IBOutlet private weak var plusOneButton: UIButton!
+    
+    @IBOutlet private weak var restartButton: UIButton!
+    
+    @IBOutlet private weak var infoTextView: UITextView!
+    
+    // Методы класса
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        infoCounter.text = "История изменений:"
+        infoTextView.text = "История изменений:"
     }
+    
+    // IBAction на кнопки "+1", "-1", "сбросить счетчик"
+    
+    @IBAction private func plusOne(_ sender: Any) {
+        counter += 1
+        mainCounter.text = "\(counter)"
+        infoTextView.text.append("\n[\(Date().formatted())]: значение изменено на +1")
+    }
+    
+    @IBAction private func minusOne(_ sender: Any) {
+        if counter >= 1{
+            counter -= 1
+            mainCounter.text = "\(counter)"
+            infoTextView.text.append("\n[\(Date().formatted())]: значение изменено на -1")
+        } else {
+            infoTextView.text.append("\n[\(Date().formatted())]: попытка уменьшить значение счётчика ниже 0")
+        }
+    }
+    
+    @IBAction private func restart(_ sender: Any) {
+        counter = 0
+        mainCounter.text = "0"
+        infoTextView.text.append("\n[\(Date().formatted())]: значение сброшено")
+   }
+    
+    
+    
 
-    
-    
-   
+
 }
 
